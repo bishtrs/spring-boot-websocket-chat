7282a9b1278ff7a32a83104ae495db70d8f5be8d
Rohit Bisht <bishtrs@yahoo.com>
2025-04-13 12:06:52 +0530

    added logger

--- src/main/java/com/bisht/example/Application.java
+++ src/main/java/com/bisht/example/Application.java
@@ -1,11 +1,20 @@
 package com.bisht.example;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
+	
+    private static final Logger logger = LoggerFactory.getLogger(Application.class);
+
+	
 	public static void main(String[] args) {
+        logger.info("Starting Spring Boot WebSocket Chat Application...");
 		SpringApplication.run(Application.class, args);
+        logger.info("Spring Boot WebSocket Chat Application started successfully.");
+
 	}
 }
