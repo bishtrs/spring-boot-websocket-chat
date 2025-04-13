f638cea7af247646e9624a2f961092760fe26e81
Rohit Bisht <bishtrs@yahoo.com>
2025-04-13 11:55:04 +0530

    added junit

--- src/main/java/com/bisht/example/controller/ChatController.java
+++ src/main/java/com/bisht/example/controller/ChatController.java
@@ -1,5 +1,7 @@
 package com.bisht.example.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
@@ -9,13 +11,18 @@ import com.bisht.example.model.Chat;
 
 @Controller
 public class ChatController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(ChatController.class);
+
 
 	@MessageMapping("/chat")
 	@SendTo("/topic/chat")
 	public Chat greeting(@Payload Chat chat) {
 		if (chat == null || chat.getMessage() == null || chat.getUser() == null) {
+			logger.error("Invalid chat data received");
 			throw new IllegalArgumentException("Invalid chat data received");
 		}
+        logger.info("Received chat message from {}: {}", chat.getUser(), chat.getMessage());
 		return chat;
 	}
 
